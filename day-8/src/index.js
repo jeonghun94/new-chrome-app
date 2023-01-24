d4fd28ece187a3fd4aa16d720303fd1ac3978f31
조정훈 <jeongh1021@naver.com>
2023-01-24 15:16:30 +0900

    노마더 10주스터디 3주차 JS 7일차 과제 자료

---
+++ day-8/src/index.js
@@ -0,0 +1,16 @@
+const clockTitle = document.querySelector(".js-clock");
+
+const calculateDate = () => {
+  const now = new Date();
+  const eve = new Date(`2023-12-24:00:00:00`);
+
+  const gap = eve - now;
+  const seconds = Math.floor((gap / 1000) % 60);
+  const minutes = Math.floor((gap / 1000 / 60) % 60);
+  const hours = Math.floor((gap / 1000 / 60 / 60) % 24);
+  const days = Math.floor(gap / 1000 / 60 / 60 / 24);
+
+  clockTitle.innerText = `${days}d ${hours}h ${minutes}m ${seconds}s`;
+};
+
+setInterval(calculateDate, 1000);
